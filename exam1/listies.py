f5dd2c0bb8430bc57448e9dba0dbd59246b0dae1
Peter <peter@Zipcoders-MacBook-Pro-6.local>
2024-07-22 09:58:27 -0400

    append an prepend a list

--- exam1/listies.py
+++ exam1/listies.py
@@ -29,6 +29,10 @@ def add_item_front(lst, a):
     This function takes a list and an item,
     returning the list with the item prepended to the list
     """
+    # prepend/start
+    lst.insert(0,a)
+    return lst
+
     pass  # implement me
 
 def add_item_end(lst, a):
@@ -36,17 +40,23 @@ def add_item_end(lst, a):
     This function takes a list and an item,
     returning the list with the item appended to the list
     """
+    
+    # lst.insert(len(lst-1),a)
+    lst.insert(len(lst),a)
+    return lst
     pass  # implement me
 
 def drop_first_two(lst):
     """
     This function takes a list and returns a list with the first two items removed.
     """
+
     pass  # implement me
 
 def drop_last_two(lst):
     """
     This function takes a list and returns a list with the last two items removed.
     """
+    
     pass  # implement me
 
